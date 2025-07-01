377dde3882902a02b9acae15b57b7ff44908e860
qLong <nguyenquanglong100285@gmail.com>
2025-07-01 14:59:24 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="app-container">
+        hello word
+        <button className="btn btn-primary"> test</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
